70d90b22463fdf9232232afd5fd4288e250be68f
silasdani <danielsilas23@yahoo.com>
2021-11-23 13:37:41 +0200

    added GET request locations by param user_id

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,17 @@
 class LocationsController < ApplicationController
     before_action :logged_in_user
+    before_action :correct_user, only: [:show]
 
     def locations 
       @locations = @current_user.locations
 
       render json: LocationSerializer.new(@locations).serialized_json
+    end
+
+    def show
+        user = User.find(params[:id])
+        @locations = user.locations;
+        render json: LocationSerializer.new(@locations).serialized_json
     end
 
     def add_location
@@ -19,5 +26,11 @@ class LocationsController < ApplicationController
 
       def location_params
         params.require(:location).permit(:location_title, :street_number, :locality, :postal_code, :latitude, :longitude, :place_id, :country, :facility_name)
+      end
+
+      # Confirms the correct user.
+      def correct_user
+        @user = User.find(params[:id])
+        render json: { "message": "You can't do that" }, status: 322 unless (current_user?(@user) || current_user.admin? || current_user.manager?)
       end
 end

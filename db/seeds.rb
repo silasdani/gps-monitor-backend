2e592ea03abca4d792e4bbbd4f212270572043d6
silasdani <danielsilas23@yahoo.com>
2021-11-24 15:10:03 +0200

    altered the seeds & removed locations from users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-User.create!(name: "Admin",
+User.create!(name: "GM Silas",
              email: "admin@yahoo.com",
-             password: "foobar",
-             password_confirmation: "foobar",
+             password: "password",
+             password_confirmation: "password",
              admin: true,
              activated: true)
-User.create!(name: "Manager",
+User.create!(name: "M Silas",
              email: "manager@yahoo.com",
-             password: "foobar",
-             password_confirmation: "foobar",
+             password: "password",
+             password_confirmation: "password",
              manager: true,
              activated: true)
-User.create!(name: "User",
+User.create!(name: "Silas Daniel",
              email: "user@yahoo.com",
-             password: "foobar",
-             password_confirmation: "foobar",
+             password: "password",
+             password_confirmation: "password",
              activated: true)
 
 
 # Generate a bunch of additional users.
-27.times do |n|
+15.times do |n|
   name = Faker::Name.name
-  email = "example-#{n + 1}@yahoo.com"
+  email = "user-#{n + 1}@yahoo.com"
   password = "password"
   User.create!(name: name,
                email: email,
@@ -30,17 +30,18 @@ User.create!(name: "User",
 end
 
 # Generate jogging tracks for a subset of users.
-users = User.order(:created_at).take(25)
-20.times do
-  date = Faker::Time.between_dates(from: Date.today.prev_month - 1, to: Date.today, period: :day) #=> "2014-09-19 08:07:52 -0700"
-  distance = Faker::Number.between(from: 2.0, to: 8.0).round(2)
-  time = Faker::Number.within(range: 600..2000) # seconds
-  location = Faker::Address.full_address
-
-  # location_title: "La mine in sat" street_number: Faker:: locality postal_code:integer latitude longitude place_id country facility_name
-  users.each { |user|
+users = User.order(:created_at).take(10)
+do  users.each { |user|
+  5.times 
+    date = Faker::Time.between_dates(from: Date.today.prev_month - 1, to: Date.today, period: :day) #=> "2014-09-19 08:07:52 -0700"
+    distance = Faker::Number.between(from: 2.0, to: 8.0).round(2)
+    time = Faker::Number.within(range: 600..2000) # seconds
+    location = Faker::Address.full_address
     user.tracks.create!(date: date, distance: distance, time: time, location: location)
+    
     address = Faker::Address
-    user.locations.create!(location_title: address.full_address, street_number:  address.building_number, locality: address.city, postal_code: address.zip_code, latitude: address.latitude, longitude: address.longitude, place_id: address.secondary_address, facility_name: address.community)
+    lat = Faker::Number.between(from: 46.76, to: 46.78)
+    lng = Faker::Number.between(from: 23.55, to: 23.69)
+    user.locations.create!(location_title: address.full_address, street_number:  address.building_number, locality: address.city, postal_code: address.zip_code, latitude: lat, longitude: lng, place_id: address.secondary_address, facility_name: address.community)
   }
 end

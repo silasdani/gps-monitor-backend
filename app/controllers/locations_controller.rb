79a4ad0f7a34dbf6b91accb2749909fa3a387b0f
silasdani <danielsilas23@yahoo.com>
2021-11-24 22:27:35 +0200

    getting locations filtered by date

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,28 +9,43 @@ class LocationsController < ApplicationController
     end
 
     def show
-        user = User.find(params[:id])
-        @locations = user.locations;
-        render json: LocationSerializer.new(@locations).serialized_json
+      user = User.find(params[:id])
+      @locations = user.locations;
+      render json: LocationSerializer.new(@locations).serialized_json
     end
 
     def add_location
-        if @current_user.locations.create!(location_params)
-          render json: LocationSerializer.new(@current_user.locations.last).serialized_json
-        elsif
-          render json: { erroe: "Location hasn't been added!" }, status: 422
-        end
+      if @current_user.locations.create!(location_params)
+        render json: LocationSerializer.new(@current_user.locations.last).serialized_json
+      elsif
+        render json: { erroe: "Location hasn't been added!" }, status: 422
       end
+    end
 
-      private
+    def get_user_locations 
+      user = User.find(params[:id])
+      start_time = date_params[:start_time]
+      end_time = date_params[:end_time]
+      @locations = user.locations
+      @locations = @locations.where("created_at >= ? AND created_at <= ?", start_time, end_time);
 
-      def location_params
-        params.require(:location).permit(:location_title, :street_number, :locality, :postal_code, :latitude, :longitude, :place_id, :country, :facility_name)
-      end
+      render json: LocationSerializer.new(@locations).serialized_json
+    end
 
-      # Confirms the correct user.
-      def correct_user
-        @user = User.find(params[:id])
-        render json: { "message": "You can't do that" }, status: 322 unless (current_user?(@user) || current_user.admin? || current_user.manager?)
-      end
+    private
+
+    def location_params
+      params.require(:location).permit(:location_title, :street_number, :locality, :postal_code, :latitude, :longitude, :place_id, :country, :facility_name)
+    end
+
+    def date_params
+      params.require(:date).permit(:start_time, :end_time)
+    end
+
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      render json: { "message": "You can't do that" }, status: 322 unless (current_user?(@user) || current_user.admin? || current_user.manager?)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post 'validate_token', to: 'password_resets#validate_token'
   post 'reset_password', to: 'password_resets#update'
   post 'location', to: 'locations#add_location'
+  post 'user_locations/:id', to: 'locations#get_user_locations'
+  post 'location', to: 'locations#add_location'
   get 'locations', to: 'locations#locations'
   get 'tracks/my', to: 'tracks#my'
   get 'tracks/weekly_report', to: 'tracks#weekly_report'
